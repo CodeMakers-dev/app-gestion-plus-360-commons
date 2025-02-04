32cc856bc9a7c4910fbdb4f4f4f4aec1c1d7648a
celuladevarquitectura <celuladev.arquitectura@col.bellota.com>
2025-02-04 15:07:31 -0500

    Primer commit - subiendo el proyecto

---
+++ src/test/java/app_gestion_plus_360_api/AppGestionPlus360ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app_gestion_plus_360_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestionPlus360ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
