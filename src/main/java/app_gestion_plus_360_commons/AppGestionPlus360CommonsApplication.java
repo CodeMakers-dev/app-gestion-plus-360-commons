5e1055227ef7c27ceb93f85fc62da038d6293faa
celuladevarquitectura <celuladev.arquitectura@col.bellota.com>
2025-02-04 15:44:23 -0500

    primer commit subiendo el repositorio

---
+++ src/main/java/app_gestion_plus_360_commons/AppGestionPlus360CommonsApplication.java
@@ -0,0 +1,13 @@
+package app_gestion_plus_360_commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestionPlus360CommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestionPlus360CommonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app_gestion_plus_360_commons/AppGestionPlus360CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package app_gestion_plus_360_commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestionPlus360CommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
