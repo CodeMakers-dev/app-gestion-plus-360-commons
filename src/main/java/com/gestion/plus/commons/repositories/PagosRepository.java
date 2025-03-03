2a1123c6a2451370c953b9f95f8ecd64da9f31c6
celuladevarquitectura <celuladev.arquitectura@col.bellota.com>
2025-03-03 17:48:35 -0500

    Se agraga modificacion en el repository de pagos agregando metodo de findByPersona

--- src/main/java/com/gestion/plus/commons/repositories/PagosRepository.java
+++ src/main/java/com/gestion/plus/commons/repositories/PagosRepository.java
@@ -1,11 +1,16 @@
 package com.gestion.plus.commons.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gestion.plus.commons.entities.PagosEntity;
+import com.gestion.plus.commons.entities.PersonaEntity;
 
 @Repository
 public interface PagosRepository extends JpaRepository<PagosEntity, Integer> {
+	
+	Optional<PagosEntity> findByPersona(PersonaEntity persona);
 
 }
